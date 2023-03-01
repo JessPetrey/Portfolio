c85a19817950490bd56c0951ef854a7b0ce7f3cf
Jessica Petrey <jesipetrey@gmail.com>
2023-03-01 13:35:14 -0800

    init

---
+++ client/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="min-h-100vh" data-theme='garden'>
+            About
+        </div>
+    )
+}
+
+export default About
---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: ['light','dark','luxury','cupcake','bumblebee','garden','black'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+}
