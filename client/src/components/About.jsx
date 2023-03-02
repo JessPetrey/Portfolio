c88f51e300723b94e7709f94d858d62dbe9bc039
Jessica Petrey <jesipetrey@gmail.com>
2023-03-01 16:24:51 -0800

    profile img onMouseOver

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className="min-h-screen p-5">
-            <h3 className='text-5xl'>About</h3>
-            <h2>Who's Jess?</h2>
+        <div className="min-h-screen p-10 prose">
+            <h3 className='text-2xl'>About</h3>
+            <h2 className='text-4xl'>Who's Jess?</h2>
+            <div>
+                <img src={require('../images/Jess-selfie.jpeg')} alt='Jess Petrey' className='max-w-sm rounded-lg shadow-2xl'></img>
+            </div>
 
         </div>
     )
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,10 @@ const Home = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className='hero-content flex-col lg:flex-row-reverse'>
+                    <img className=" grayscale max-w-md rounded-lg shadow-2xl" src={require('../images/Jess-selfie.jpeg')} alt='Jess Petrey'
+                        onMouseOver={e => e.currentTarget.className='grayscale-0 max-w-md rounded-lg shadow-2xl'} onMouseOut={e => e.currentTarget.className='grayscale max-w-md rounded-lg shadow-2xl'} />
+                </div>
                 <div>
                     <h1 className="text-5xl font-bold">Hi! I'm Jess</h1>
                     <p className="py-6 text-3xl">I'm a Full Stack Developer and Fiber Artist</p>{/* add js to rotate through different texts! */}
