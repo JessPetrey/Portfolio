fbcd79b8f3e98afd705c0d0e47dc06637f036962
Jessica Petrey <jesipetrey@gmail.com>
2023-05-16 18:32:23 -0700

    add validations to contact form, success+load msgs

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,22 +1,68 @@
-import React, { useRef } from 'react'
-import emailjs, { init } from "@emailjs/browser";
+import React, { useRef, useState } from 'react';
+import emailjs, { init as initEmailJS } from '@emailjs/browser';
 
 const Contact = (props) => {
-    init("user_xxxxxxxxxxxxxxxxxxx");
+    initEmailJS("user_xxxxxxxxxxxxxxxxxxx");
     const form = useRef();
+    const [errors, setErrors] = useState({});
+    const [messageSubmitted, setMessageSubmitted] = useState(false);
+    const [messageLoading, setMessageLoading] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        emailjs.sendForm("service_dfsa0qo", "template_re8ft6h", form.current, "IV7bzbNOADkc3p_XA")
-            .then((result) => {
-                alert("Message Sent Successfully");
-                console.log(result.text);
-            },
-                (error) => {
-                    console.log(error.text);
-                }
-            );
+        if (validateForm()) {
+            setMessageLoading(true);
+            emailjs
+                .sendForm("service_dfsa0qo", "template_re8ft6h", form.current, "IV7bzbNOADkc3p_XA")
+                .then(
+                    (result) => {
+                        setMessageSubmitted(true);
+                        setTimeout(resetForm, 5000);
+                    },
+                    (error) => {
+                        console.log(error.text);
+                    }
+                )
+                .finally(() => {
+                    setMessageLoading(false);
+                });
+        }
     };
+
+    const resetForm = () => {
+        form.current.reset();
+        setErrors({});
+        setMessageSubmitted(false);
+    };
+
+    const validateForm = () => {
+        const nameInput = form.current.name;
+        const emailInput = form.current.email;
+        const messageInput = form.current.message;
+        const errors = {};
+
+        // Name validation
+        if (nameInput.value.trim() === '') {
+            errors.name = 'Please enter your name';
+        }
+
+        // Email validation
+        if (emailInput.value.trim() === '') {
+            errors.email = 'Please enter your email';
+        } else if (!/\S+@\S+\.\S+/.test(emailInput.value)) {
+            errors.email = 'Please enter a valid email address';
+        }
+
+        // Message validation
+        if (messageInput.value.trim() === '') {
+            errors.message = 'Please enter a message';
+        }
+
+        setErrors(errors);
+        // Return true if there are no errors, otherwise false
+        return Object.keys(errors).length === 0;
+    };
+
     return (
         <div className="hero min-h-screen">
             <div className='hero-content gap-20 prose' ref={props.contact}>
@@ -24,18 +70,30 @@ const Contact = (props) => {
                     <h3 className='text-lg'>CONTACT</h3>
                     <h2 className='text-2xl'>Get In Touch</h2>
                     <div>
-                        <p>Feel free to contact me if you have any questions, or just want to say hi!</p>
+                        <p>Feel free to contact me if you have any questions or just want to say hi!</p>
                         <form ref={form} onSubmit={handleSubmit} className='form-control'>
-                            <input type="text" name="name" placeholder='Your Name' className='my-2' />
-                            <input type="email" name="email" placeholder='Your Email' className='my-2' />
-                            <textarea name="message" cols="30" rows="8" placeholder='Message'></textarea>
-                            <button type="submit" className='btn'>Send Email</button>
+                            <input type="text" name="name" placeholder='Your Name' className='my-2' required />
+                            {errors.name && <span className="error">{errors.name}</span>}
+                            <input type="email" name="email" placeholder='Your Email' className='my-2' required />
+                            {errors.email && <span className="error">{errors.email}</span>}
+                            <textarea name="message" cols="30" rows="8" placeholder='Message' required></textarea>
+                            {errors.message && <span className="error">{errors.message}</span>}
+                            {/* on successful submit - conditionally render a loading message, success message,
+                            or "send email" */}
+                            {messageSubmitted ? (
+                                <strong className='btn btn-primary'>Message Sent!</strong>
+                            ) : (
+                                <button type="submit" className='btn' disabled={messageLoading}>
+                                    {messageLoading ?
+                                        'Sending...' : 'Send Email'}
+                                </button>
+                            )}
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
